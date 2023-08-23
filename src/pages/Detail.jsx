aa716bd098413af74cf3bd85e0b8048a1f444650
furkanpolat06 <emnplt.0698@gmail.com>
2023-08-23 13:46:58 +0300

    detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,27 @@
+import { CardContent, Typography } from '@mui/material'
 import React from 'react'
 
 export const Detail = () => {
   return (
-    <div>Detail</div>
+    <div>
+      <CardContent>
+    <Typography paragraph>İçerik</Typography>
+    <Typography paragraph>
+    Ankara Kalesi, Ankara'nın Altındağ ilçesinde bulunan tarihi bir kale. Ne zaman yapıldığı kesin olarak bilinmemekle birlikte M.Ö. 5. yüzyıl başında Galatların Ankara'ya yerleşmeleri sırasında kalenin var olduğu bilinmektedir. Romalılar, Bizanslılar, Selçuklu Hanedanı ve Osmanlılar dönemlerinde birçok kez onarımdan geçmiştir. Ankara Kalesi dışarıdan görümüne göre daha büyüktür. Her yıl çeşitli festivallere de ev sahipliği yapmaktadır.
+    </Typography>
+    
+    <Typography paragraph>
+      Add rice and stir very gently to distribute. Top with artichokes and
+      peppers, and cook without stirring, until most of the liquid is absorbed,
+      15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+      mussels, tucking them down into the rice, and cook again without
+      stirring, until mussels have opened and rice is just tender, 5 to 7
+      minutes more. (Discard any mussels that don&apos;t open.)
+    </Typography>
+    <Typography>
+      Set aside off of the heat to let rest for 10 minutes, and then serve.
+    </Typography>
+  </CardContent>
+  </div>
   )
 }

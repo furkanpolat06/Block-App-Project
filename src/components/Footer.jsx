ca68188b83e04794887ffb3bad0048a0dd8d5efe
furkanpolat06 <emnplt.0698@gmail.com>
2023-08-23 09:49:31 +0300

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer style={{background:"lightgreen", textAlign:"center", padding:"1rem"}}>Developed by FS Team Copyright ©  <a href="https://github.com/furkanpolat06">FizyoWeb</a></footer>
   )
 }
 
